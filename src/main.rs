4a199d30f6a1fcb24bdc390b9c0d313fe4d33e3c
Michal Franc <lam.michal.franc@gmail.com>
2019-03-31 02:01:18 +0100

    Better error handling with failure::Fail

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,65 @@
 use quicli::prelude::*;
 use structopt::StructOpt;
 use std::process::Command;
+
+use std::io;
+
 use git2::Repository;
 use git2::RepositoryOpenFlags;
+
+use failure::Fail;
+ 
+use derive_more::From as DeriveFrom;
 
 #[derive(Debug, StructOpt)]
 struct Cli {
     cmd: String,
 }
 
-fn get_local_github_url() -> String {
+#[derive(Debug, DeriveFrom, Fail)]
+pub enum Error {
+    #[fail(display = "git2 error - {}", _0)]
+    Git2Error(#[cause] git2::Error),
+    
+    #[fail(display = "No origin url found")]
+    OriginUrlNotFound,
 
-    let v = vec!["/home"];
+    #[fail(display = "command error: {}", _0)]
+    IoError(#[cause] io::Error),
+}
 
-    let repo = match Repository::open_ext(".", RepositoryOpenFlags::empty(), v) {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to open: {}", e),
-    };
-
-    let origin = match repo.find_remote("origin") {
-        Ok(origin) => origin,
-        Err(e) => panic!("remote origin not found: {}", e),
-    };
-
-    let origin_git_url = match origin.url() {
-        Some(url) => url,
-        None => panic!("No origin url found"),
-    };
-
-    println!("found remote origin: {:?}", origin_git_url);
-
+fn get_local_github_url() -> Result<String, Error> {
+    let repo = Repository::open_ext(".", RepositoryOpenFlags::empty(), vec!["/home"])?;
+    let origin = repo.find_remote("origin")?;
+    let origin_git_url = origin.url().ok_or(Error::OriginUrlNotFound)?;
     let origin_https = origin_git_url.replace(".git", "").replace(":", "/").replace("git@", "https://");
-    return origin_https;
+    return Ok(origin_https);
 }
 
-fn travis() {
-    let travis_url = get_local_github_url().replace("github.com", "travis-ci.org");
-    Command::new("xdg-open").arg(travis_url).spawn();
+fn travis() -> Result<(), Error>{
+    let github_url = get_local_github_url()?;
+    let travis_url = github_url.replace("github.com", "travis-ci.org");
+    let mut process = Command::new("xdg-open").arg(travis_url).spawn().unwrap();
+    process.wait().expect("waiting for command to finish");
+    println!("travis has opened sucessfully");
+
+    Ok(())
 }
 
-fn github() {
-    Command::new("xdg-open").arg(get_local_github_url()).spawn();
+fn github() -> Result<(), Error> {
+    let github_url = get_local_github_url()?;
+    let mut process = Command::new("xdg-open").arg(github_url).spawn()?;
+    process.wait().expect("waiting for command to finish");
+    println!("github has opened sucessfully");
+
+    Ok(())
 }
 
 fn main() -> CliResult {
     let args = Cli::from_args();
     match args.cmd.as_ref() {
-        "github" => github(),
-        "travis" => travis(),
+        "github" => github()?,
+        "travis" => travis()?,
         _ => println!("Not supported command"),
     }
 
